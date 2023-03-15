7c040ec9a5e85153ca0060b96d80bd1cc64716d1
Neonlab_kr <ksknk67@gmail.com>
2023-03-15 19:07:36 +0900

    Fast_I/O

---
+++ Baekjun/15552.cpp
@@ -0,0 +1,15 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	int count, num1, num2;
+//	cin.tie(NULL);
+//	cin.sync_with_stdio(false);
+//	cin >> count;
+//	for (int i = 0;i < count;i++) {
+//		cin >> num1 >> num2;
+//		cout << num1 + num2 << "\n";
+//	}
+//	return 0;
+//}
