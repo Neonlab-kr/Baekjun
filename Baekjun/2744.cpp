02572de0ba519f0783d48f954db51338ef17967c
Neonlab-kr <92604834+Neonlab-kr@users.noreply.github.com>
2023-03-17 22:22:38 +0900

    Neonlab_kr
    String

---
+++ Baekjun/2744.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string input;
+    cin >> input;
+    for(int i=0;i<input.length();i++){
+        if(int(input[i])>96)
+            input[i]=char(int(input[i])-32);
+        else
+            input[i]=char(int(input[i])+32);
+    }
+    cout << input;
+    return 0;
+}
