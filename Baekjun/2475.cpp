a7c0e1a6c6a2aa262205098b898f7dc4d7461990
Neonlab_kr <ksknk67@gmail.com>
2023-03-18 16:31:46 +0900

    Functions

---
+++ Baekjun/2475.cpp
@@ -0,0 +1,13 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <iostream>
+//using namespace std;
+//
+//int main() {
+//	int input, authnum = 0;
+//	for (int i = 0;i < 5;i++) {
+//		cin >> input;
+//		authnum = (authnum + input*input) % 10;
+//	}
+//	cout << authnum;
+//	return 0;
+//}
