e1b5b1115f3caf9e2b03557558c8c0649c9eee4e
Neonlab_kr <ksknk67@gmail.com>
2023-03-21 14:21:13 +0900

    class2

---
+++ Baekjun/1085.cpp
@@ -0,0 +1,12 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <iostream>
+//#include <algorithm>
+//using namespace std;
+//
+//int main() {
+//	int x, y, w, h;
+//	cin >> x >> y >> w >> h;
+//
+//	cout << min(x, (min(y, min(w - x, h - y))));
+//	return 0;
+//}
