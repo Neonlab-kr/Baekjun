9bf36f37c2eed49440d81fbc18c98d853de0b7d5
Neonlab_kr <ksknk67@gmail.com>
2023-03-21 14:50:54 +0900

    class2

---
+++ Baekjun/1259.cpp
@@ -0,0 +1,24 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//
+//int main() {
+//	string str,output;
+//	while (true) {
+//		cin >> str;
+//		if (str == "0")
+//			break;
+//
+//		output = "yes";
+//		for (int i = 0;i < str.length()/2+1;i++) {
+//			if (*(str.begin() + i) != *(str.end() - i - 1)) {
+//				output = "no";
+//			}
+//		}
+//		cout << output << '\n';
+//	}
+//
+//	return 0;
+//}
