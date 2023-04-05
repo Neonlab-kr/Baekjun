5c7ccb79f1e3b6e9dbceaac8b6c96a8959208464
Neonlab_kr <ksknk67@gmail.com>
2023-04-05 21:54:49 +0900

    class3

---
+++ Baekjun/11399.cpp
@@ -0,0 +1,23 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<iostream>
+//#include<algorithm>
+//using namespace std;
+//
+//int n, i;
+//int p[1000];
+//int main(void) {
+//	ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+//	cin >> n;
+//	for (int i = 0; i < n;i++) {
+//		cin >> p[i];
+//	}
+//
+//	sort(p, p + n);
+//	int sum = 0;
+//	for (int i = 0; i < n;i++) {
+//		sum += p[i] * (n - i);
+//	}
+//
+//	cout << sum << endl;
+//	return 0;
+//}
