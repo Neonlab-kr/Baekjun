76d6efd56b2dc75507863b3499e67297812eb54f
Neonlab_kr <ksknk67@gmail.com>
2023-04-15 21:49:01 +0900

    bronze

---
+++ Baekjun/11022.cpp
@@ -0,0 +1,16 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+//	int a, b, T;
+//	cin >> T;
+//	for (int i = 0;i < T;i++) {
+//		cin >> a >> b;
+//		cout << "Case #" << i + 1 << ": " << a << " + " << b << " = " << a + b << '\n';
+//	}
+//
+//	return 0;
+//}
