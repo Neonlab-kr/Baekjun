e31a83458b7e4d90ead9b094cdb22bbf91be76d6
Neonlab_kr <ksknk67@gmail.com>
2023-04-18 06:40:37 +0900

    bronze

---
+++ Baekjun/3003.cpp
@@ -0,0 +1,18 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+//
+//	int chess[6] = { 1,1,2,2,2,8 };
+//	int tmp;
+//
+//	for (int i : chess) {
+//		cin >> tmp;
+//		cout << i - tmp << ' ';
+//	}
+//
+//	return 0;
+//}
