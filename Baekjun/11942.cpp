a192bc35d57b472e87c380370f0de3cf4c64eaec
Neonlab_kr <ksknk67@gmail.com>
2023-04-22 19:16:42 +0900

    bronze

---
+++ Baekjun/11942.cpp
@@ -0,0 +1,10 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+//	cout << "고려대학교";
+//	return 0;
+//}
