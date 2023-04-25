1af79d487704f6f1349fa8178481eb0eefa65b38
Neonlab_kr <ksknk67@gmail.com>
2023-04-26 00:03:56 +0900

    bronze

---
+++ Baekjun/10926.cpp
@@ -0,0 +1,16 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int main()
+//{
+//	ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+//
+//	string inputString;
+//
+//	cin >> inputString;
+//
+//	cout << inputString << "??!";
+//	return 0;
+//}
