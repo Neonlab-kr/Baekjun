501303e2e5a049e5354426b97edeed7362a4f456
Neonlab_kr <ksknk67@gmail.com>
2023-04-29 17:23:48 +0900

    bronze

--- Baekjun/10039.cpp
+++ Baekjun/10039.cpp
@@ -1,13 +1,13 @@
-#include<iostream>
-using namespace std;
-
-int main()
-{
-    int sum=0, tmp;
-    for(int i=0;i<5;i++){
-        cin>>tmp;
-        if(tmp>40) sum+=tmp;
-        else sum+=40;
-    }
-    cout<<sum/5;
-}
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//    int sum=0, tmp;
+//    for(int i=0;i<5;i++){
+//        cin>>tmp;
+//        if(tmp>40) sum+=tmp;
+//        else sum+=40;
+//    }
+//    cout<<sum/5;
+//}
---
+++ Baekjun/5596.cpp
@@ -0,0 +1,25 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <iostream>
+//#include <algorithm>
+//using namespace std;
+//
+//int main()
+//{
+//	ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+//
+//	int sum1 = 0, sum2 = 0;
+//	int tmp;
+//
+//	for (int i = 0;i < 4;i++) {
+//		cin >> tmp;
+//		sum1 += tmp;
+//	}
+//
+//	for (int i = 0;i < 4;i++) {
+//		cin >> tmp;
+//		sum2 += tmp;
+//	}
+//
+//	cout << max(sum1,sum2);
+//	return 0;
+//}
