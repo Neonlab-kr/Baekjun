a413de96b1fd4f67b9cbdd09cba98248e65d0a06
Neonlab_kr <ksknk67@gmail.com>
2023-04-30 09:26:40 +0900

    bronze

---
+++ Baekjun/10808.cpp
@@ -0,0 +1,10 @@
+//#include <iostream>
+//#include <string>
+//int main(int i)
+//{
+//	int a[26] = { 0, };
+//	std::string w;
+//	std::cin >> w;
+//	for (i = 0;i < w.length();a[w[i++] - 'a']++);
+//	for (i = 0;i < 26;std::cout << a[i++] << ' ');
+//}
