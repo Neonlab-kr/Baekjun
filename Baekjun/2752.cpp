563dc1ac16ee680784d609cabecd10be918da965
Neonlab_kr <ksknk67@gmail.com>
2023-05-01 19:44:42 +0900

    bronze

---
+++ Baekjun/2752.cpp
@@ -0,0 +1,11 @@
+//#include <iostream>
+//#include <algorithm>
+//using namespace std;
+//
+//int main()
+//{
+//	int numArray[3];
+//	for (int i = 0;i < 3;cin >> numArray[i++]);
+//	sort(numArray, numArray + 3);
+//	for (int i = 0;i < 3;cout << numArray[i++] << ' ');
+//}
