82d555140ef9fc7df393c22a0a0590a8fbaac2d5
Neonlab_kr <ksknk67@gmail.com>
2023-05-02 21:46:03 +0900

    bronze

---
+++ Baekjun/15059.cpp
@@ -0,0 +1,17 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int food[3], request[3], sum = 0;
+//
+//	for (int i = 0;i < 3;i++) cin >> food[i];
+//	for (int i = 0;i < 3;i++) cin >> request[i];
+//
+//	for (int i = 0;i < 3;i++) {
+//		int tmp = food[i] - request[i];
+//		if (tmp < 0) sum -= tmp;
+//	}
+//
+//	cout << sum;
+//}
