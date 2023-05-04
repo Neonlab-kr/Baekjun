bdedb1781b88fadaf80b45f2aae79d8d9c854a23
Neonlab_kr <ksknk67@gmail.com>
2023-05-05 06:13:17 +0900

    bronze

---
+++ Baekjun/1032.cpp
@@ -0,0 +1,19 @@
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int main()
+//{
+//	int i;
+//	string origin;
+//	char tmp;
+//	cin >> i >> origin;
+//
+//	for (;i > 1;i--) {
+//		for (int j = 0;j < origin.length();j++) {
+//			cin >> tmp;
+//			if (tmp != origin[j]) origin[j] = '?';
+//		}
+//	}
+//	cout << origin;
+//}
