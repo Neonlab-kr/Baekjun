35b01561d51522a2737042348cd44ef4b7e10710
Neonlab_kr <ksknk67@gmail.com>
2023-05-04 20:56:05 +0900

    bronze

---
+++ Baekjun/2845.cpp
@@ -0,0 +1,14 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int population, tmp;
+//	cin >> population >> tmp;
+//	population *= tmp;
+//
+//	for(int i = 0;i < 5;i++) {
+//		cin >> tmp;
+//		cout << tmp - population << ' ';
+//	}
+//}
