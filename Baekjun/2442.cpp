661e989aefe658c02d3b0ac1b7568f81fa88f000
Neonlab_kr <ksknk67@gmail.com>
2023-05-07 09:02:33 +0900

    bronze

---
+++ Baekjun/2442.cpp
@@ -0,0 +1,16 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int N, empty, star = 1;
+//	cin >> N;
+//	empty = N - 1;
+//	for (int i = 0;i < N;i++) {
+//		for (int j = 0;j < empty;j++) cout << ' ';
+//		for (int j = 0;j < star;j++) cout << '*';
+//		empty--;
+//		star += 2;
+//		cout << '\n';
+//	}
+//}
