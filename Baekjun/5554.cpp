8e2e94b2ffe143644c98cc312bd4bddc12a607d4
Neonlab_kr <ksknk67@gmail.com>
2023-05-11 15:46:10 +0900

    bronze

---
+++ Baekjun/5554.cpp
@@ -0,0 +1,12 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int total = 0, tmp;
+//	for (int i = 0;i < 4;i++) {
+//		cin >> tmp;
+//		total += tmp;
+//	}
+//	cout << total / 60 << '\n' << total % 60;
+//}
