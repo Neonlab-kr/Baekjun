6febb82ef42d8727c63367811d57b04231db26c0
Neonlab_kr <ksknk67@gmail.com>
2023-05-13 09:28:18 +0900

    bronze

---
+++ Baekjun/11365.cpp
@@ -0,0 +1,15 @@
+//#include <iostream>
+//#include <algorithm>
+//#include <string>
+//using namespace std;
+//
+//int main()
+//{
+//	string tmp;
+//	while (true) {
+//		getline(cin, tmp);
+//		if (tmp == "END") break;
+//		reverse(tmp.begin(), tmp.end());
+//		cout << tmp << '\n';
+//	}
+//}
