948a17a5bce37b9a21e1e7841093d8720b93f1bd
Neonlab_kr <ksknk67@gmail.com>
2023-05-16 14:03:16 +0900

    bronze

---
+++ Baekjun/2588.cpp
@@ -0,0 +1,11 @@
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int main() {
+//	int num1;
+//	string num2;
+//	cin >> num1 >> num2;
+//
+//	cout << num1 * (num2[2] - '0') << '\n' << num1 * (num2[1] - '0') << '\n' << num1 * (num2[0] - '0') << '\n' << num1 * stoi(num2);
+//}
