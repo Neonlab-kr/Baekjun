8eecc9561f123ec65b889d11a94924e1228b7280
Neonlab_kr <ksknk67@gmail.com>
2023-05-18 04:02:47 +0900

    bronze

---
+++ Baekjun/2857.cpp
@@ -0,0 +1,29 @@
+//#include <iostream>
+//#include <algorithm>
+//#include <string>
+//#include <vector>
+//
+//using namespace std;
+//
+//int main(void) {
+//	vector<string> str(5);
+//	string fbi = "FBI";
+//	vector<int> num;
+//
+//
+//	for (auto &c : str) {
+//		cin >> c;
+//	}
+//
+//	for (int i = 0; i < 5; i++) {
+//		if (str[i].find(fbi, 0) != -1) {
+//			num.push_back(i + 1);
+//		}
+//	}
+//
+//	if (num.size() == 0) cout << "HE GOT AWAY!";
+//	else {
+//		for (auto c : num) cout << c << " ";
+//	}
+//
+//}
