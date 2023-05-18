08ec8e25b7727ae7cf0aaf13a919cccde17e9958
Neonlab_kr <ksknk67@gmail.com>
2023-05-18 09:50:39 +0900

    bronze

---
+++ Baekjun/2446.cpp
@@ -0,0 +1,18 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int N;
+//	cin >> N;
+//	for (int i = 0;i < N;i++) {
+//		for (int j = 0;j < i;j++) cout << ' ';
+//		for (int j = 0;j < (N - i) * 2 - 1;j++) cout << '*';
+//		cout << '\n';
+//	}
+//	for (int i = N - 1;i > 0;i--) {
+//		for (int j = 0;j < i - 1;j++) cout << ' ';
+//		for (int j = 0;j < (N - i) * 2 + 1;j++) cout << '*';
+//		cout << '\n';
+//	}
+//}
