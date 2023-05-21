7b8f5a73d9455e603c34a6687e6d5223491a40a8
Neonlab_kr <ksknk67@gmail.com>
2023-05-21 09:41:02 +0900

    bronze

---
+++ Baekjun/10953.cpp
@@ -0,0 +1,14 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int N, tmp1, tmp2;
+//	cin >> N;
+//	for (int i = 0;i < N;i++) {
+//		cin >> tmp1;
+//		cin.ignore();
+//		cin >> tmp2;
+//		cout << tmp1 + tmp2 << '\n';
+//	}
+//}
