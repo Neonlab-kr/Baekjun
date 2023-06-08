9990960f7024055be00226fb87bcf078e7c9504f
Neonlab_kr <ksknk67@gmail.com>
2023-06-08 15:39:08 +0900

    silver

---
+++ Baekjun/1941.cpp
@@ -0,0 +1,18 @@
+//#include <iostream>
+//#include <string>
+//#include <regex>
+//using namespace std;
+//
+//int main() {
+//	string input = "";
+//	cin >> input;
+//	input = regex_replace(input, regex("c="), "a");
+//	input = regex_replace(input, regex("c-"), "a");
+//	input = regex_replace(input, regex("dz="), "a");
+//	input = regex_replace(input, regex("d-"), "a");
+//	input = regex_replace(input, regex("lj"), "a");
+//	input = regex_replace(input, regex("nj"), "a");
+//	input = regex_replace(input, regex("s="), "a");
+//	input = regex_replace(input, regex("z="), "a");
+//	cout << input.length();
+//}
