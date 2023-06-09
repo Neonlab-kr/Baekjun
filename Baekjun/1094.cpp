7e8b7c301b25f966dec6af9775d82e028440d229
Neonlab_kr <ksknk67@gmail.com>
2023-06-09 13:51:58 +0900

    silver

---
+++ Baekjun/1094.cpp
@@ -0,0 +1,12 @@
+//#include <iostream>
+//#include <bitset>
+//#include <regex>
+//using namespace std;
+//
+//int main() {
+//	int X;
+//	string binary;
+//	cin >> X;
+//	binary = bitset<7>(X).to_string();
+//	cout << binary.length() - regex_replace(binary, regex("1"), "").length();
+//}
