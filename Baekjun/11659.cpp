2be4260ce73ebb38c8111ee7342b13c5af9af449
Neonlab_kr <ksknk67@gmail.com>
2023-06-14 08:14:48 +0900

    class3

---
+++ Baekjun/11659.cpp
@@ -0,0 +1,28 @@
+//#include<iostream>
+//
+//using namespace std;
+//
+//int N, M;
+//int map[100001];
+//int dp[100001];
+//
+//int main() {
+//	ios::sync_with_stdio(0);
+//	cin.tie(0);
+//
+//	cin >> N >> M;
+//
+//	for (int i = 1; i <= N; i++) {
+//		int a;
+//		cin >> a;
+//		map[i] = a;
+//		dp[i] = a + dp[i - 1];
+//	}
+//
+//	while (M--) {
+//		int a, b;
+//		cin >> a >> b;
+//
+//		cout << dp[b] - dp[a - 1] << '\n';
+//	}
+//}
