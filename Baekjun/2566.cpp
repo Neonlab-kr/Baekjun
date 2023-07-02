31eb69b8d876b947fa5f7ac89a2efc0e2da38beb
Neonlab_kr <ksknk67@gmail.com>
2023-07-02 13:05:23 +0900

    bronze

---
+++ Baekjun/2566.cpp
@@ -0,0 +1,18 @@
+//#include <iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int a, n, m, sum = 0;
+//	for (int i = 1; i <= 9; i++) {
+//		for (int j = 1; j <= 9; j++) {
+//			cin >> a;
+//			if (a >= sum) {
+//				sum = a;
+//				n = i;
+//				m = j;
+//			}
+//		}
+//	}
+//	cout << sum << endl << n << ' ' << m << endl;
+//}
